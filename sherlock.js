91a772eb58dbae4145f1931050914a648b48693c
Guillermodelrioao <guillermo.delrioao@udlap.mx>
2015-09-01 12:12:43 -0500

    squarenumber.js

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,23 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+
+INPUT = "2\n3 9\n17 24";//The inicial data
+console.log(INPUT);
+console.log();
+var res = INPUT.split("\n");//elemenate the \n
+arr=res;//Copy the data to the array
+cas = parseInt(res[0]);//Convert the data in int
+for(b=1;b<=cas;b++){
+	var temp=arr[b].split(" ");//Separets the data 
+	console.log(cuadradoPerfecto(parseInt(temp[0]),parseInt(temp[1])));//Call the function and count
+}
+function cuadradoPerfecto(ini,fin){
+	x=0;//declares the counter
+	for(i=ini;i<=fin;i++){
+		if(Math.sqrt(i)%1==0){//Compare if the data is a Square number
+		x++;
+		}
+}
+return x;
+}
